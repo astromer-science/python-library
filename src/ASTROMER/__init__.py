265acab8b39f0566b5c27a5c0c2b8c3b022e41ea
cristobal <cridonoso@inf.udec.cl>
2022-12-02 13:17:30 -0300

    update

--- src/ASTROMER/__init__.py
+++ src/ASTROMER/__init__.py
@@ -3,6 +3,6 @@ __path__ = extend_path(__path__, __name__)
 import sys, os
 print(sys.path)
 
-sys.path.append(os.path.join(sys.path[-1] 'core'))
+sys.path.append(os.path.join(sys.path[-1], 'core'))
 
 __version__ = "0.0.12"

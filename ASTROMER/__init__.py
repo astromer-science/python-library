49ef5a5f7020c9bf6860de06dec5de23dfb10329
cristobal <cridonoso@inf.udec.cl>
2023-01-24 16:21:56 -0300

    update

--- ASTROMER/__init__.py
+++ ASTROMER/__init__.py
@@ -1,8 +1,1 @@
-import sys, os
 
-print('-------------- PATHS -----------')
-print(sys.path)
-sys.path.append(os.path.join(sys.path[-1], 'core'))
-print('--------------------------------')
-
-__version__ = "0.0.20"
